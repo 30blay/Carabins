14066bdf8ae0aca5bf42a721fb2b28918f5bcc0f
etienne <etienne.tremblay@polymtl.ca>
2019-05-16 10:41:00 -0400

    Neural net

--- regression.py
+++ regression.py
@@ -1,6 +1,6 @@
 from analysis import get_subject_metrics
 from sklearn.model_selection import train_test_split
-from sklearn import svm, linear_model
+from sklearn import svm, linear_model, neural_network
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_validate
@@ -23,11 +23,11 @@ models = [
     linear_model.Lasso(),
     linear_model.SGDRegressor(),
     svm.SVR(gamma='auto'),
+    neural_network.MLPRegressor((4,), max_iter=20000)
 ]
 
 for model in models:
     cv_results = cross_validate(model, X, y, cv=3, scoring=scoring, return_train_score=True)
     print(model.__class__)
-    print(cv_results['test_r2'].mean())
-
-
+    print('Train R2: ', cv_results['train_r2'].mean(), '    std: ', cv_results['train_r2'].std())
+    print('Test R2: ', cv_results['test_r2'].mean(), '    std: ', cv_results['test_r2'].std())
